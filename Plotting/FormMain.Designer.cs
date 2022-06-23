3d8375fe4a63d8f5c5d06aa99672e5a763113354
CaptainNH <stas08072002@gmail.com>
2022-06-23 11:14:48 +0300

    func

--- Plotting/FormMain.Designer.cs
+++ Plotting/FormMain.Designer.cs
@@ -28,41 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.label1 = new System.Windows.Forms.Label();
             this.comboBoxFunctions = new System.Windows.Forms.ComboBox();
             this.splitter1 = new System.Windows.Forms.Splitter();
             this.labelFunction = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.buttonBuild = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
             this.SuspendLayout();
             // 
             // chart
             // 
-            chartArea2.AxisX.MajorTickMark.Interval = 0D;
-            chartArea2.AxisX.Maximum = 10D;
-            chartArea2.AxisX.Minimum = -10D;
-            chartArea2.AxisY.Maximum = 10D;
-            chartArea2.AxisY.Minimum = -10D;
-            chartArea2.Name = "ChartArea1";
-            this.chart.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart.Legends.Add(legend2);
-            this.chart.Location = new System.Drawing.Point(21, 198);
-            this.chart.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.chart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea3.AxisX.MajorTickMark.Interval = 0D;
+            chartArea3.AxisX.Maximum = 10D;
+            chartArea3.AxisX.Minimum = -10D;
+            chartArea3.AxisY.Maximum = 10D;
+            chartArea3.AxisY.Minimum = -10D;
+            chartArea3.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart.Legends.Add(legend3);
+            this.chart.Location = new System.Drawing.Point(44, 234);
+            this.chart.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.chart.Name = "chart";
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chart.Series.Add(series2);
-            this.chart.Size = new System.Drawing.Size(862, 405);
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart.Series.Add(series3);
+            this.chart.Size = new System.Drawing.Size(1149, 498);
             this.chart.TabIndex = 0;
             this.chart.Text = "chart1";
             // 
@@ -70,90 +70,70 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(61, 13);
+            this.label1.Location = new System.Drawing.Point(81, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(197, 25);
+            this.label1.Size = new System.Drawing.Size(244, 29);
             this.label1.TabIndex = 1;
             this.label1.Text = "Выберите функцию";
             // 
             // comboBoxFunctions
             // 
             this.comboBoxFunctions.FormattingEnabled = true;
-            this.comboBoxFunctions.Location = new System.Drawing.Point(64, 54);
+            this.comboBoxFunctions.Location = new System.Drawing.Point(85, 66);
+            this.comboBoxFunctions.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBoxFunctions.Name = "comboBoxFunctions";
-            this.comboBoxFunctions.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxFunctions.Size = new System.Drawing.Size(160, 24);
             this.comboBoxFunctions.TabIndex = 2;
+            this.comboBoxFunctions.SelectedValueChanged += new System.EventHandler(this.comboBoxFunctions_SelectedValueChanged);
             // 
             // splitter1
             // 
             this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.splitter1.Name = "splitter1";
-            this.splitter1.Size = new System.Drawing.Size(3, 627);
+            this.splitter1.Size = new System.Drawing.Size(4, 772);
             this.splitter1.TabIndex = 3;
             this.splitter1.TabStop = false;
             // 
             // labelFunction
             // 
+            this.labelFunction.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelFunction.AutoSize = true;
             this.labelFunction.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelFunction.Location = new System.Drawing.Point(353, 46);
+            this.labelFunction.Location = new System.Drawing.Point(426, 35);
+            this.labelFunction.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelFunction.Name = "labelFunction";
-            this.labelFunction.Size = new System.Drawing.Size(357, 55);
+            this.labelFunction.Size = new System.Drawing.Size(0, 58);
             this.labelFunction.TabIndex = 4;
-            this.labelFunction.Text = "a * f(b * x + c) + d";
             // 
-            // textBox1
+            // buttonBuild
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(361, 49);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(31, 41);
-            this.textBox1.TabIndex = 5;
-            this.textBox1.Text = "1";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox2.Location = new System.Drawing.Point(443, 54);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(31, 41);
-            this.textBox2.TabIndex = 6;
-            this.textBox2.Text = "1";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox3.Location = new System.Drawing.Point(561, 54);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(31, 41);
-            this.textBox3.TabIndex = 7;
-            this.textBox3.Text = "0";
-            // 
-            // textBox4
-            // 
-            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox4.Location = new System.Drawing.Point(643, 54);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(31, 41);
-            this.textBox4.TabIndex = 8;
-            this.textBox4.Text = "0";
+            this.buttonBuild.Location = new System.Drawing.Point(580, 154);
+            this.buttonBuild.Name = "buttonBuild";
+            this.buttonBuild.Size = new System.Drawing.Size(175, 34);
+            this.buttonBuild.TabIndex = 5;
+            this.buttonBuild.Text = "Построить график";
+            this.buttonBuild.UseVisualStyleBackColor = true;
             // 
             // FormMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(932, 627);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1243, 772);
+            this.Controls.Add(this.buttonBuild);
             this.Controls.Add(this.labelFunction);
             this.Controls.Add(this.splitter1);
             this.Controls.Add(this.comboBoxFunctions);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.chart);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormMain";
             this.Text = "Построение графиков";
+            this.Load += new System.EventHandler(this.FormMain_Load);
             ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -167,10 +147,7 @@
         private System.Windows.Forms.ComboBox comboBoxFunctions;
         private System.Windows.Forms.Splitter splitter1;
         private System.Windows.Forms.Label labelFunction;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button buttonBuild;
     }
 }
 
--- Plotting/FormMain.cs
+++ Plotting/FormMain.cs
@@ -10,11 +10,48 @@ using System.Windows.Forms;
 
 namespace Plotting
 {
+
     public partial class FormMain : Form
     {
+        private string[] Functions =
+        {
+            "sin",
+            "cos",
+            "tan",
+            "cot",
+            "arcsin",
+            "arccos",
+            "arctan",
+            "arccot",
+            $"x{(char)8319}",
+            "a^x",
+            "log"
+        };
+
         public FormMain()
         {
             InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            comboBoxFunctions.Items.AddRange(Functions);
+            
+        }
+
+        private void comboBoxFunctions_SelectedValueChanged(object sender, EventArgs e)
+        {
+            var index = comboBoxFunctions.SelectedIndex;
+            if (index <= 7)
+            {
+                labelFunction.Text = $"a + {Functions[index]}(b * x + c) + d";                
+            }
+            else if (index == 8)
+                labelFunction.Text = $"a * x{(char)8319} + b";
+            else if (index == 9)
+                labelFunction.Text = $"a^((k * x) + b) + c";
+            else
+                labelFunction.Text = $"a * log((k * x + b), n) + c";
         }
     }
 }
